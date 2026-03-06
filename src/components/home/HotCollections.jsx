5ea79cc5b5d5d54720ce0d8ca0adeb8de03b71e5
gerthor <ger.thor97@gmail.com>
2026-03-05 18:06:45 -0800

    Implemented Hot Collections: Axios API Call, Dynamic Data, Carousel, Skeleton Loading State, Responsive Resolution

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,59 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
-import axios from "axios"
+import axios from "axios";
 import Slider from "react-slick";
 
-
-
 const HotCollections = () => {
-  const [dataHotCollections, setDataHotCollections] = useState([])
-
+  const [dataHotCollections, setDataHotCollections] = useState([]);
+  const [loadingHotCollection, setLoadingHotCollection] = useState(true);
 
   function NextArrow(props) {
     const { className, onClick } = props;
-    return (
-      <div
-        className={className}
-        onClick={onClick}
-      />
-    );
+    return <div className={className} onClick={onClick} />;
   }
 
   function PrevArrow(props) {
     const { className, onClick } = props;
-    return (
-      <div
-        className={className}
-        onClick={onClick}
-      />
-    );
+    return <div className={className} onClick={onClick} />;
   }
 
-
-
   var settings = {
-    dots: false, // false
+    dots: false,
     infinite: true,
     speed: 100,
     slidesToShow: 4,
     slidesToScroll: 1,
-    swipe: false,
     initialSlide: 0,
-    // lazyLoad: true,
-    // variableWidth: true,
-    adaptiveHeight: false,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
-    responsive:[
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-        }
+        },
       },
       {
         breakpoint: 768,
@@ -61,7 +41,7 @@ const HotCollections = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-        }
+        },
       },
       {
         breakpoint: 480,
@@ -69,23 +49,22 @@ const HotCollections = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-        }
-      }
-    ] 
+        },
+      },
+    ],
   };
 
   async function getHotCollectionsAPI() {
-    const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
-    setDataHotCollections(data)
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections",
+    );
+    setDataHotCollections(data);
   }
 
   useEffect(() => {
-    getHotCollectionsAPI()
-  }, [])
-
-
-
-
+    getHotCollectionsAPI();
+    setLoadingHotCollection(false);
+  }, []);
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -98,30 +77,49 @@ const HotCollections = () => {
             </div>
           </div>
           <Slider {...settings}>
-            {dataHotCollections.map((collection) => ( /* Needs Skeleton loading state */
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to={`/item-details/${collection.nftId}`}>
-                      <img src={collection.nftImage} className="lazy img-fluid" alt="" />
-                    </Link>
+            {loadingHotCollection
+              ? new Array(4).fill(0).map((_, index) => (
+                  <div key={index}>
+                    <div className="nft_coll nft_coll_skeleton">
+                      <div className="nft_wrap"></div>
+                      <div className="nft_coll_pp">
+                      </div>
+                      <div className="nft_coll_info"></div>
+                    </div>
                   </div>
-                  <div className="nft_coll_pp">
-                    <Link to="/author">
-                      <img className="lazy pp-coll" src={collection.authorImage} alt="" />
-                    </Link>
-                    <i className="fa fa-check"></i>
+                ))
+              : dataHotCollections.map((collection) => (
+                  <div key={collection.id}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to={`/item-details/${collection.nftId}`}>
+                          <img
+                            src={collection.nftImage}
+                            className="lazy img-fluid"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img
+                            className="lazy pp-coll"
+                            src={collection.authorImage}
+                            alt=""
+                          />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{collection.title}</h4>
+                        </Link>
+                        <span>ERC-{collection.code}</span>
+                      </div>
+                    </div>
                   </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{collection.title}</h4>
-                    </Link>
-                    <span>ERC-{collection.code}</span>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Slider>
+                ))}
+          </Slider>
         </div>
       </div>
     </section>
